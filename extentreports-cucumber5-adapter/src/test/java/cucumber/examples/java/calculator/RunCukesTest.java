421e0f55d0ed88cbbb5ba397d4f8cc1e6ec5ff9f
grasshopper7 <grass.hopper.moc@gmail.com>
2020-03-22 14:44:40 +0530

    Updated jar

--- extentreports-cucumber5-adapter/src/test/java/cucumber/examples/java/calculator/RunCukesTest.java
+++ extentreports-cucumber5-adapter/src/test/java/cucumber/examples/java/calculator/RunCukesTest.java
@@ -1,14 +1,11 @@
 package cucumber.examples.java.calculator;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import org.junit.runner.RunWith;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 //@Category(Example.class)
-public class RunCukesTest /* extends AbstractTestNGCucumberTests */{
+public class RunCukesTest  extends AbstractTestNGCucumberTests {
 
 }
